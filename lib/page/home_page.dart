8ada0543060888644137a8a52468337af311593c
liuweicom <15779712623@163.com>
2019-08-10 22:39:57 +0800

    homePage代码整理

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flex_bus/modal/user_info_model.dart';
 import 'package:flex_bus/util/navigator_util.dart';
 import 'package:flex_bus/widget/drawer_widget.dart';
-import 'package:flex_bus/widget/webview_bridge.dart';
 import 'package:flex_bus/widget/webview_bridge_in_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +16,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   TabController _controller;
   List<String> _tab = ["弹性公交", "预约公交"];
-//  DrawerController drawerController  = DrawerController(child: DrawerWidget(), alignment: DrawerAlignment.start);
   @override
   void initState() {
     // TODO: implement initState
@@ -26,6 +24,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   }
 
   void _handlerDrawerButton(BuildContext context) {
+    //控制抽屉打开
     Scaffold.of(context).openDrawer();
   }
 
@@ -41,61 +40,64 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           child: Column(
             children: <Widget>[
               _appBar(),
-              Container(
-                color: Colors.white,
-                child: FractionallySizedBox(
-                  widthFactor: 1,
-                  child: TabBar(
-                    controller: _controller,
-                    isScrollable: false,
-                    labelColor: Colors.orange,
-                    unselectedLabelColor: Colors.grey,
-                    indicator:
-                        UnderlineTabIndicator(borderSide: BorderSide.none),
-                    onTap: (index) {
-                      print(index.toString() + "index=========");
-                    },
-                    tabs: _tab.map((item) => _tabItem(item)).toList(),
-                  ),
-                ),
-              ),
-              Flexible(
-                child: Container(
-                  color: Colors.grey,
-                  child: TabBarView(
-                    controller: _controller,
-                    children: <Widget>[
-                      FlexPage(),
-                      OrderPage(),
-                    ],
-                    physics: ScrollPhysics(),
-                  ),
-                ),
-              ),
-              RaisedButton(
-                  child: Text("弹性公交"),
-                  onPressed: () {
-//                  NavigatorUtil.push(context, WebView(
-//                    url: "http://ipts.zpmc.com/et-mobile/#/login",//http://ipts.zpmc.com/ids-admin/#/login
-//                    isNewPage: true,
-//                    title: "背景",
-//                  ));
-//                  NavigatorUtil.push(context, WebViewBridge(
-//                    url: "https://flutter.dev",//http://ipts.zpmc.com/ids-admin/#/login // "http://ipts.zpmc.com/et-mobile/#/login"
-//                    isNewPage: true,
-//                    title: "背景",
-//                  ));
-                    NavigatorUtil.push(
-                        context,
-                        WebViewBridgeInPage(
-                          url:
-                              "https://flutter.dev", //http://ipts.zpmc.com/ids-admin/#/login // "http://ipts.zpmc.com/et-mobile/#/login"
-                          isNewPage: true,
-                          title: "背景",
-                        ));
-                  }),
+              _tabBarItem(),
+              _tabBarViewItem(),
+              _webViewButton(context),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _webViewButton(BuildContext context) {
+    return RaisedButton(
+      child: Text("弹性公交"),
+      onPressed: () {
+        NavigatorUtil.push(
+          context,
+          WebViewBridgeInPage(
+            url:
+                "https://flutter.dev", //http://ipts.zpmc.com/ids-admin/#/login // "http://ipts.zpmc.com/et-mobile/#/login"
+            isNewPage: true,
+            title: "背景",
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _tabBarViewItem() {
+    return Flexible(
+      child: Container(
+//      color: Colors.grey,
+        child: TabBarView(
+          controller: _controller,
+          children: <Widget>[
+            FlexPage(),
+            OrderPage(),
+          ],
+          physics: ScrollPhysics(),
+        ),
+      ),
+    );
+  }
+
+  Widget _tabBarItem() {
+    return Container(
+      color: Colors.white,
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: TabBar(
+          controller: _controller,
+          isScrollable: false,
+          labelColor: Colors.orange,
+          unselectedLabelColor: Colors.grey,
+          indicator: UnderlineTabIndicator(borderSide: BorderSide.none),
+          onTap: (index) {
+            print(index.toString() + "index=========");
+          },
+          tabs: _tab.map((item) => _tabItem(item)).toList(),
         ),
       ),
     );
@@ -112,76 +114,15 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               children: <Widget>[
                 Padding(
                   padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  child: Consumer(
-                    builder: (context, UserInfoModel userInfo, child)=>GestureDetector(
-                      onTap: (){
-                        print("print--person----");
-                        bool isLoginSuccess = userInfo.isLoginSuccess ?? false;
-                        print("isLoginSuccess:"+isLoginSuccess.toString()+"===================");
-                        if(isLoginSuccess){
-                          _handlerDrawerButton(context);
-                        }else{
-                          Navigator.pushNamed(context, "login");
-                        }
-                      },
-                      child: child,
-                    ),
-                    child: Icon(
-                      Icons.person,
-                      size: 26,
-                      color: Colors.black,
-                    ),
-                  ),
+                  child: _appBarLeftIcon(),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Center(
-                    child: GestureDetector(
-                      onTap: () {
-                        print("switch-city---------");
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text("上海市"),
-                          Icon(
-                            Icons.expand_more,
-                            size: 20,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  child: _appBarCenterTitle(),
                 ),
                 Container(
-                  child: Row(
-                    children: <Widget>[
-                      GestureDetector(
-                        child: Icon(
-                          Icons.message,
-                          size: 20,
-                          color: Colors.black,
-                        ),
-                        onTap: () {
-                          print("message==========");
-                        },
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            left: 20, top: 0, right: 10, bottom: 0),
-                        child: GestureDetector(
-                          child: Icon(
-                            Icons.crop_free,
-                            size: 20,
-                            color: Colors.black,
-                          ),
-                          onTap: () {
-                            print("scanner=========");
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
+                  padding: EdgeInsets.only(right: 10),
+                  child: _appBarRightIcons(),
                 ),
               ],
             ),
@@ -189,19 +130,88 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     );
   }
 
+  _appBarRightIcons() {
+    return Row(
+      children: <Widget>[
+        GestureDetector(
+          child: Icon(
+            Icons.message,
+            size: 20,
+            color: Colors.black,
+          ),
+          onTap: () {
+            print("message==========");
+          },
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            left: 20,
+            top: 0,
+            right: 0,
+            bottom: 0,
+          ),
+          child: GestureDetector(
+            child: Icon(
+              Icons.crop_free,
+              size: 20,
+              color: Colors.black,
+            ),
+            onTap: () {
+              print("scanner=========");
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  _appBarCenterTitle() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          print("switch-city---------");
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("上海市"),
+            Icon(
+              Icons.expand_more,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _appBarLeftIcon() {
+    return Consumer(
+      builder: (context, UserInfoModel userInfo, child) => GestureDetector(
+            onTap: () {
+              bool isLoginSuccess = userInfo.isLoginSuccess ?? false;
+              print("isLoginSuccess:" +
+                  isLoginSuccess.toString() +
+                  "===================");
+              if (isLoginSuccess) {
+                _handlerDrawerButton(context);
+              } else {
+                Navigator.pushNamed(context, "login");
+              }
+            },
+            child: child,
+          ),
+      child: Icon(
+        Icons.person,
+        size: 26,
+        color: Colors.black,
+      ),
+    );
+  }
+
   Widget _tabItem(String s) {
     return Tab(
       text: s,
     );
-  }
-
-  _personIconTap(UserInfoModel userInfo) {
-    print("print--person----");
-    bool isLoginSuccess = userInfo.isLoginSuccess;
-    if(isLoginSuccess ?? false){
-      Navigator.pushNamed(context, "login");
-    }else{
-      _handlerDrawerButton(context);
-    }
   }
 }
